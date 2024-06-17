1cc5b8e24b8d37d1e05a56e3590c1dfa37b81394
Coelor <Coelortv@gmail.com>
2024-06-16 23:08:53 -0500

    Created repo. Added code. Need to start creating new GUI.

---
+++ CraftingAdditions/CraftingAdditionsMod.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Config;
+
+namespace CraftingAdditions
+{
+
+    public class CraftingAdditionsMod : ModSystem
+    {
+        ICoreClientAPI capi;
+        ICoreServerAPI sapi;
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            if (api.Side == EnumAppSide.Server)
+            {
+                sapi = api as ICoreServerAPI;
+            }
+            else
+            {
+                capi = api as ICoreClientAPI;
+            }
+            api.RegisterBlockClass("BlockAnvilNew", typeof(BlockAnvilNew));
+            api.RegisterBlockEntityClass("BlockEntityAnvilNew", typeof(BEAnvilNew));
+        }
+    }
+}
