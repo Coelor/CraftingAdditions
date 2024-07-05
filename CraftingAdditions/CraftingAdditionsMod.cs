307b8cbe1a1f4c3c37d5d78efa852682bb186a79
Coelor <coelortv@gmail.com>
2024-07-05 02:02:23 -0500

    Made monkey patch. Easier. Works.

--- CraftingAdditions/CraftingAdditionsMod.cs
+++ CraftingAdditions/CraftingAdditionsMod.cs
@@ -2,28 +2,56 @@
 using Vintagestory.API.Client;
 using Vintagestory.API.Server;
 using Vintagestory.API.MathTools;
+using System;
+using System.Collections.Generic;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
 using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using HarmonyLib;
+using Vintagestory.Client.NoObf;
 
-namespace CraftingAdditions
-{
+namespace CraftingAdditions {
+    public class CraftingAdditionsMod : ModSystem {
+        private ICoreAPI _api;
+        private ICoreClientAPI _capi;
+        private ICoreServerAPI _sapi;
+        private string _ModID;
 
-    public class CraftingAdditionsMod : ModSystem
-    {
-        ICoreClientAPI capi;
-        ICoreServerAPI sapi;
-        public override void Start(ICoreAPI api)
-        {
-            base.Start(api);
-            if (api.Side == EnumAppSide.Server)
-            {
-                sapi = api as ICoreServerAPI;
+        private Harmony _harmony;
+        public override void Start(ICoreAPI api) {
+            this._ModID = Mod.Info.ModID;
+            this._api = api;
+            
+            base.Start(this._api);
+            if (this._api.Side == EnumAppSide.Server) {
+                _sapi = this._api as ICoreServerAPI;
+            } else {
+                _capi = this._api as ICoreClientAPI;
             }
-            else
+            // this.api.RegisterBlockClass("BlockAnvilNew", typeof(BlockAnvilNew));
+            // this.api.RegisterBlockEntityClass("BlockEntityAnvilNew", typeof(BEAnvilNew));
+            
+            if(!Harmony.HasAnyPatches(_ModID))
             {
-                capi = api as ICoreClientAPI;
+                _harmony = new Harmony(_ModID);
+                _harmony.PatchAll();
             }
-            api.RegisterBlockClass("BlockAnvilNew", typeof(BlockAnvilNew));
-            api.RegisterBlockEntityClass("BlockEntityAnvilNew", typeof(BEAnvilNew));
         }
+
+        public override void Dispose()
+        {
+            _harmony?.UnpatchAll(_ModID);
+        }
+        
+        //Test Patch
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(EntityItem), "ReceiveDamage")]
+        public void ItemReceivedDamageAfter(EntityItem __instance, float damage) {
+            this._api.Logger.Event("{0} took {1} damage!", __instance.Slot.Itemstack, damage);
+            this._api.Logger.Event("Is it still alive? {0}", __instance.Alive);
+        }
+        
+        
     }
 }
---
+++ CraftingAdditions/Patches/AnvilRecipeSelectorGUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using Vintagestory.GameContent;
+using System.Reflection.Emit;
+using Vintagestory.API.Common;
+using System.Linq;
+
+namespace CraftingAdditions.Patches
+{
+    [HarmonyPatch]
+    public class AnvilRecipeSelectorGUI : ModSystem
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(GuiDialogBlockEntityRecipeSelector), "SetupDialog")]
+        public static IEnumerable<CodeInstruction> TranspileSetupDialog(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count - 1; i++)
+            {
+                if (codes[i].opcode == OpCodes.Dup && codes[i + 1].opcode == OpCodes.Ldc_I4_7 &&
+                    codes[i + 2].opcode == OpCodes.Call && codes[i + 3].opcode == OpCodes.Stloc_0)
+                {
+                    codes[i + 1].opcode = OpCodes.Ldc_I4;
+                    codes[i + 1].operand = 14;
+                }
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+}
